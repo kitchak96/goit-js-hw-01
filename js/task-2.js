2915e91049116258353d4938a4d99609a1af12a5
Olha Goriachko <kitchak96@gmail.com>
2024-08-26 19:10:09 +0200

    all5

--- js/task-2.js
+++ js/task-2.js
@@ -4,3 +4,4 @@ function getShippingMessage (country, price, deliveryFee) {
     console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
     console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
     console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+    <script src="./js/task-2.js"></script>
--- js/task-3.js
+++ js/task-3.js
@@ -8,3 +8,4 @@ function getElementWidth (content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+<script src="./js/task-3.js"></script>
